519a9cdacbb8fc6b56a739561ce1159100f8f0c1
henriquenicolli <henrique.nicolli@gmail.com>
2018-08-23 20:39:47 -0300

    teste

--- src/test/java/Mediana/MedianaTest.java
+++ src/test/java/Mediana/MedianaTest.java
@@ -1,10 +1,11 @@
+package Mediana;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-import com.mycompany.atividade06.Mediana;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -12,8 +13,8 @@ import static org.junit.Assert.*;
  *
  * @author Aluno
  */
-public class MedianaTeste {
-    //projeto alterado
+public class MedianaTest {
+    
     @Test
     public void MedianaTeste() {
         double[] array = {1,2,3,4};
